2304d2683458a1be498bb56d5382698e49b4f0fa
Oussema <ousemabenamor100@outlook.fr>
2019-04-16 17:31:24 +0100

    01 Creating the Domain Models

---
+++ Supermarket.Domain/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supermarket.Domain.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public short QuantityInPackage { get; set; }
+        public EUnitOfMeasurement UnitOfMeasurement { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
